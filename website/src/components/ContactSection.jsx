830d21e488665e61da1aaa1413265d5a8ed7b671
Robistyle <robi9.lel@gmail.com>
2024-07-26 14:23:19 +0300

    Some details & bugs fixed

--- website/src/components/ContactSection.jsx
+++ website/src/components/ContactSection.jsx
@@ -2,88 +2,114 @@
 
 import React from "react";
 import "./ContactSection.css";
+import Swal from "sweetalert2";
+
 const ContactSection = () => {
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "daaa8d73-a7b0-4da9-bc38-abf232e8664e");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      Swal.fire({
+        title: "Success!",
+        text: "Message sent succsessfully!",
+        icon: "success",
+      });
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
   return (
-    <div className='ContactSection'>
-      <section className='contact'>
-        <div className='content'>
-          <h2>Contact Us</h2>
-          <p>Feel free to reach out to us for any queries or suggestions.</p>
-        </div>
-        <div className='container'>
-          <div className='contactInfo'>
-            <div className='box'>
-              <div className='icon'>
-                <b></b>
-              </div>
-              <div className='text'>
-                <h3>Phone</h3>
-                <p>1069350</p>
-              </div>
-            </div>
-            <div className='box'>
-              <div className='icon'>
-                <b></b>
-                <i class='fa-regular fa-envelope'></i>
-              </div>
-              <div className='text'>
-                <h3>Email</h3>
-                <p>akfmwakfa@gmail.com</p>
-              </div>
-            </div>
-            <h2 className='txt'>Let's connect!</h2>
-            <ul className='sci'>
-              <li>
-                <a href='#'>
-                  <i class='fa-brands fa-facebook'></i>
-                </a>
-              </li>
-              <li>
-                <a href='#'>
-                  <i class='fa-brands fa-instagram'></i>
-                </a>
-              </li>
-              <li>
-                <a href='#'>
-                  <i class='fa-brands fa-github'></i>
-                </a>
-              </li>
-              <li>
-                <a href='#'>
-                  <i class='fa-brands fa-linkedin'></i>
-                </a>
-              </li>
-              <li>
-                <a href='#'></a>
-              </li>
-            </ul>
+    <section id='contact-section'>
+      <div className='ContactSection'>
+        <section className='contact'>
+          <div className='content'>
+            <h2>Let's get in touch!</h2>
+            <p>Feel free to reach out</p>
           </div>
-          <div className='contactForm'>
-            <form>
-              <h2>Send Message</h2>
-              <div className='inputBox'>
-                <input type='text' name='' requierd='required' />
-                <span>Full Name</span>
+          <div className='container'>
+            <div className='contactInfo'>
+              <div className='box'>
+                <div className='icon'>
+                  <b></b>
+                  <i class='fa-regular fa-envelope'></i>
+                </div>
+                <div className='text'>
+                  <h3>Email</h3>
+                  <p>leliuc.robert@gmail.com</p>
+                </div>
               </div>
+              <h2 className='txt'>Let's connect!</h2>
+              <ul className='sci'>
+                <li>
+                  <a
+                    href='https://www.facebook.com/robi.lel.7/'
+                    target='_blank'
+                  >
+                    <i class='fa-brands fa-facebook'></i>
+                  </a>
+                </li>
+                <li>
+                  <a href='#'>
+                    <i class='fa-brands fa-instagram'></i>
+                  </a>
+                </li>
+                <li>
+                  <a href='https://github.com/LeliucRobert' target='_blank'>
+                    <i class='fa-brands fa-github'></i>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href='https://linkedin.com/in/leliuc-robert-997a35232'
+                    target='_blank'
+                  >
+                    <i class='fa-brands fa-linkedin'></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className='contactForm'>
+              <form onSubmit={onSubmit}>
+                <h2>Send Message</h2>
+                <div className='inputBox'>
+                  <input type='text' name='name' required='required' />
+                  <span>Full Name</span>
+                </div>
 
-              <div className='inputBox'>
-                <input type='text' name='' requierd='required' />
-                <span>Email</span>
-              </div>
+                <div className='inputBox'>
+                  <input type='text' name='email' required='required' />
+                  <span>Email</span>
+                </div>
 
-              <div className='inputBox'>
-                <textarea required='required'></textarea>
-                <span>Type your Message...</span>
-              </div>
+                <div className='inputBox'>
+                  <textarea required='required' name='message'></textarea>
+                  <span>Type your Message...</span>
+                </div>
 
-              <div className='inputBox'>
-                <input type='submit' value='Send' />
-              </div>
-            </form>
+                <div className='inputBox'>
+                  <input type='submit' value='Send' />
+                </div>
+              </form>
+            </div>
           </div>
-        </div>
-      </section>
-    </div>
+        </section>
+      </div>
+    </section>
   );
 };
 
--- website/src/components/ProjectsSection.jsx
+++ website/src/components/ProjectsSection.jsx
@@ -15,143 +15,145 @@ import "swiper/css/scrollbar";
 
 const ProjectsSection = () => {
   return (
-    <div className='ProjectsSection'>
-      <h1 className='ProjectsLabel'>Projects</h1>
-      <Swiper
-        modules={[Navigation, Pagination, Scrollbar, A11y]}
-        spaceBetween={50}
-        slidesPerView={3}
-        navigation
-        pagination={{ clickable: true }}
-        scrollbar={{ draggable: true }}
-        onSwiper={(swiper) => console.log(swiper)}
-        onSlideChange={() => console.log("slide change")}
-        simulateTouch={true} // Enable mouse dragging
-        allowTouchMove={true} // Enable touch dragging
-      >
-        <SwiperSlide>
-          <div class='nft'>
-            <div class='main'>
-              <img
-                class='tokenImage'
-                src='https://images.unsplash.com/photo-1621075160523-b936ad96132a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
-                alt='NFT'
-              />
-              <h2>Kibertopiks #4269</h2>
-              <p class='description'>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Architecto neque laboriosam blanditiis harum eos voluptate
-                explicabo quasi eum quibusdam veritatis exercitationem ea
-                suscipit beatae vero amet, incidunt veniam nemo ducimus.
-              </p>
+    <section id='projects-section'>
+      <div className='ProjectsSection'>
+        <h1 className='ProjectsLabel'>Projects</h1>
+        <Swiper
+          modules={[Navigation, Pagination, Scrollbar, A11y]}
+          spaceBetween={50}
+          slidesPerView={3}
+          navigation
+          pagination={{ clickable: true }}
+          scrollbar={{ draggable: true }}
+          onSwiper={(swiper) => console.log(swiper)}
+          onSlideChange={() => console.log("slide change")}
+          simulateTouch={true} // Enable mouse dragging
+          allowTouchMove={true} // Enable touch dragging
+        >
+          <SwiperSlide>
+            <div class='nft'>
+              <div class='main'>
+                <img
+                  class='tokenImage'
+                  src='https://images.unsplash.com/photo-1621075160523-b936ad96132a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+                  alt='NFT'
+                />
+                <h2>Kibertopiks #4269</h2>
+                <p class='description'>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Architecto neque laboriosam blanditiis harum eos voluptate
+                  explicabo quasi eum quibusdam veritatis exercitationem ea
+                  suscipit beatae vero amet, incidunt veniam nemo ducimus.
+                </p>
 
-              <hr />
+                <hr />
+              </div>
             </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div class='nft'>
-            <div class='main'>
-              <img
-                class='tokenImage'
-                src='https://images.unsplash.com/photo-1621075160523-b936ad96132a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
-                alt='NFT'
-              />
-              <h2>Kibertopiks #4269</h2>
-              <p class='description'>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Architecto neque laboriosam blanditiis harum eos voluptate
-                explicabo quasi eum quibusdam veritatis exercitationem ea
-                suscipit beatae vero amet, incidunt veniam nemo ducimus.
-              </p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div class='nft'>
+              <div class='main'>
+                <img
+                  class='tokenImage'
+                  src='https://images.unsplash.com/photo-1621075160523-b936ad96132a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+                  alt='NFT'
+                />
+                <h2>Kibertopiks #4269</h2>
+                <p class='description'>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Architecto neque laboriosam blanditiis harum eos voluptate
+                  explicabo quasi eum quibusdam veritatis exercitationem ea
+                  suscipit beatae vero amet, incidunt veniam nemo ducimus.
+                </p>
 
-              <hr />
+                <hr />
+              </div>
             </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div class='nft'>
-            <div class='main'>
-              <img
-                class='tokenImage'
-                src='https://images.unsplash.com/photo-1621075160523-b936ad96132a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
-                alt='NFT'
-              />
-              <h2>Kibertopiks #4269</h2>
-              <p class='description'>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Architecto neque laboriosam blanditiis harum eos voluptate
-                explicabo quasi eum quibusdam veritatis exercitationem ea
-                suscipit beatae vero amet, incidunt veniam nemo ducimus.
-              </p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div class='nft'>
+              <div class='main'>
+                <img
+                  class='tokenImage'
+                  src='https://images.unsplash.com/photo-1621075160523-b936ad96132a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+                  alt='NFT'
+                />
+                <h2>Kibertopiks #4269</h2>
+                <p class='description'>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Architecto neque laboriosam blanditiis harum eos voluptate
+                  explicabo quasi eum quibusdam veritatis exercitationem ea
+                  suscipit beatae vero amet, incidunt veniam nemo ducimus.
+                </p>
 
-              <hr />
+                <hr />
+              </div>
             </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div class='nft'>
-            <div class='main'>
-              <img
-                class='tokenImage'
-                src='https://images.unsplash.com/photo-1621075160523-b936ad96132a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
-                alt='NFT'
-              />
-              <h2>Kibertopiks #4269</h2>
-              <p class='description'>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Architecto neque laboriosam blanditiis harum eos voluptate
-                explicabo quasi eum quibusdam veritatis exercitationem ea
-                suscipit beatae vero amet, incidunt veniam nemo ducimus.
-              </p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div class='nft'>
+              <div class='main'>
+                <img
+                  class='tokenImage'
+                  src='https://images.unsplash.com/photo-1621075160523-b936ad96132a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+                  alt='NFT'
+                />
+                <h2>Kibertopiks #4269</h2>
+                <p class='description'>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Architecto neque laboriosam blanditiis harum eos voluptate
+                  explicabo quasi eum quibusdam veritatis exercitationem ea
+                  suscipit beatae vero amet, incidunt veniam nemo ducimus.
+                </p>
 
-              <hr />
+                <hr />
+              </div>
             </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div class='nft'>
-            <div class='main'>
-              <img
-                class='tokenImage'
-                src='https://images.unsplash.com/photo-1621075160523-b936ad96132a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
-                alt='NFT'
-              />
-              <h2>Kibertopiks #4269</h2>
-              <p class='description'>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Architecto neque laboriosam blanditiis harum eos voluptate
-                explicabo quasi eum quibusdam veritatis exercitationem ea
-                suscipit beatae vero amet, incidunt veniam nemo ducimus.
-              </p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div class='nft'>
+              <div class='main'>
+                <img
+                  class='tokenImage'
+                  src='https://images.unsplash.com/photo-1621075160523-b936ad96132a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+                  alt='NFT'
+                />
+                <h2>Kibertopiks #4269</h2>
+                <p class='description'>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Architecto neque laboriosam blanditiis harum eos voluptate
+                  explicabo quasi eum quibusdam veritatis exercitationem ea
+                  suscipit beatae vero amet, incidunt veniam nemo ducimus.
+                </p>
 
-              <hr />
+                <hr />
+              </div>
             </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div class='nft'>
-            <div class='main'>
-              <img
-                class='tokenImage'
-                src='https://images.unsplash.com/photo-1621075160523-b936ad96132a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
-                alt='NFT'
-              />
-              <h2>Kibertopiks #4269</h2>
-              <p class='description'>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Architecto neque laboriosam blanditiis harum eos voluptate
-                explicabo quasi eum quibusdam veritatis exercitationem ea
-                suscipit beatae vero amet, incidunt veniam nemo ducimus.
-              </p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div class='nft'>
+              <div class='main'>
+                <img
+                  class='tokenImage'
+                  src='https://images.unsplash.com/photo-1621075160523-b936ad96132a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+                  alt='NFT'
+                />
+                <h2>Kibertopiks #4269</h2>
+                <p class='description'>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Architecto neque laboriosam blanditiis harum eos voluptate
+                  explicabo quasi eum quibusdam veritatis exercitationem ea
+                  suscipit beatae vero amet, incidunt veniam nemo ducimus.
+                </p>
 
-              <hr />
+                <hr />
+              </div>
             </div>
-          </div>
-        </SwiperSlide>
-        ...
-      </Swiper>
-    </div>
+          </SwiperSlide>
+          ...
+        </Swiper>
+      </div>
+    </section>
   );
 };
 
